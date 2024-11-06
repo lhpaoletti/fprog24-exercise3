d795bdf5a9e6654c71f5bac0d8e4ae10bed14f2c
Luis Henrique Paoletti <lh.paoletti@gmail.com>
2024-11-06 22:44:40 +0100

    Implemented Num for Natnum

--- src/natnum.hs
+++ src/natnum.hs
@@ -7,36 +7,68 @@ import Digit
 
 type Nat0       = Int
 type CarryDigit = Digit
+type Borrow     = Bool
+type Shift      = Nat0
 data Natnum     = L Digit           -- as in 'last digit'
                   | A Digit Natnum  -- as in 'a digit'
+type AccNatnum  = Natnum
 
 instance Show Natnum where
-  show (L d)   = show d
-  show (A d n) = show d ++ show n
+    show (L d)   = show d
+    show (A d n) = show d ++ show n
 
 instance Eq Natnum where
-  (==) (L d1)    (L d2)    = d1 == d2
-  (==) (A d1 n1) (A d2 n2) = d1 == d2 && n1 == n2
-  (==) _         _         = False
+    (==) (L d1)    (L d2)    = d1 == d2
+    (==) (A d1 n1) (A d2 n2) = d1 == d2 && n1 == n2
+    (==) _         _         = False
 
 instance Ord Natnum where
-  (<=) (L d1) (L d2) = d1 <= d2
-  (<=) n1 n2 = let ((A d1 n1'), (A d2 n2')) = pad n1 n2
-               in if d2 > d1
-                  then True
-                  else d1 <= d2 && n1' <= n2'
+    (<=) (L d1) (L d2) = d1 <= d2
+    (<=) n1 n2 =
+        let ((A d1 n1'), (A d2 n2')) = pad n1 n2
+        in if d2 > d1
+           then True
+           else d1 <= d2 && n1' <= n2'
 
 instance Enum Natnum where
-  toEnum = binary
-  fromEnum (L d)       = fromEnum d
-  fromEnum num@(A d n) = let numSize = size num
-                             factor  = 2^(numSize - 1)
-                         in (fromEnum d) * factor + (fromEnum n)
+    toEnum = binary
+    fromEnum (L d) = fromEnum d
+    fromEnum num@(A d n) =
+        let numSize = size num
+            factor  = 2^(numSize - 1)
+        in (fromEnum d) * factor + (fromEnum n)
 
 instance Num Natnum where
-  (+) n1 n2 = revert $ add (revert n1) (revert n2)
+    (+) n1 n2 = revert $ add (revert n1) (revert n2)
+    (-) n1 n2 = trunc $ sub n1 n2
+    (*) n1 n2 = trunc $ multiply n1 n2
+
+    abs = id
+    signum (A d n) =
+        if d == One
+        then (L One)
+        else signum n
+    signum l = l
+    fromInteger = binary
 
 
+{- Apply transform function n times on a Natnum. -}
+apply :: (Natnum -> Natnum) -> Int -> Natnum -> Natnum
+{- Multiply two Natnums together. -}
+multiply  :: Natnum -> Natnum -> Natnum
+multiply' :: Natnum -> Natnum -> (Shift, AccNatnum)
+{- Borrow table for subtractWithBorrow.
+ - Subtract the second Digit from the first based on the borrow of the last operation. -}
+borrowTable :: Borrow -> Digit -> Digit -> (Borrow, Digit)
+{- Subtract the second Natnum from the first using the borrow system.
+ - Both Natnums must have the same size. -}
+subtractWithBorrow :: Natnum -> Natnum -> (Borrow, Natnum)
+{- Subtract the second Natnum from the first.
+ - Result might contain leading Zeros. -}
+sub :: Natnum -> Natnum -> Natnum
+{- Truncate leading Zeros from a Natnum.
+ - 0 is truncated to 0; 00 to 0; 000 to 0; ... -}
+trunc :: Natnum -> Natnum
 {- Append a Digit at the end of a Natnum. -}
 append :: Digit -> Natnum -> Natnum
 {- Revert a Natnum. -}
@@ -50,8 +82,8 @@ add :: Natnum -> Natnum -> Natnum
 {- Build a Natnum from a list of digits. -}
 fromDigits :: [Digit] -> Natnum
 {- Transform an integer into a Natnum. -}
-binary  :: Int -> Natnum
-binary' :: [Digit] -> Int -> Natnum
+binary  :: Integral a => a -> Natnum
+binary' :: Integral a => [Digit] -> a -> Natnum
 {- Get size of a Natnum word. -}
 size :: Natnum -> Int
 {- Pad a Natnum with a fixed number of zeros. -}
@@ -59,6 +91,55 @@ padWith :: Nat0 -> Natnum -> Natnum
 {- Pad Natnums to have same size. -}
 pad :: Natnum -> Natnum -> (Natnum, Natnum)
 
+
+apply _ 0 n = n
+apply f i n = apply f (i - 1) (f n)
+
+multiply n1 n2 =
+    let (_, result) = multiply' n1 n2
+    in result
+multiply' n1 (L d) =
+    if d == One
+    then (1, n1)
+    else (1, L Zero)
+multiply' n1 (A d n2) =
+    let (shift, acc) = multiply' n1 n2
+        appendZero   = append Zero
+        applyAppendZeroShiftTimes = apply appendZero shift
+        summand      = applyAppendZeroShiftTimes n1
+    in if d == One
+       then (shift + 1, acc + summand)
+       else (shift + 1, acc)
+
+borrowTable False Zero Zero = (False, Zero)
+borrowTable False Zero One  = (True , One )
+borrowTable False One  Zero = (False, One )
+borrowTable False One  One  = (False, Zero)
+borrowTable True  Zero Zero = (True , One )  -- propagate the borrow; 0 becomes 10 (two), then 1, so it's actually 1 - 0 for the result
+borrowTable True  Zero One  = (True , Zero)  -- propagate the borrow; it's actually 1 - 1 for the result
+borrowTable True  One  Zero = (False, Zero)  -- 1 got borrowed, so it becomes 0; it's actually 0 - 0 for the result
+borrowTable True  One  One  = (True , One )  -- 1 got borrowed; it becomes 0 - 1, so a borrow is needed
+
+subtractWithBorrow (L d1) (L d2) =
+    let (borrow, d') = borrowTable False d1 d2
+    in (borrow, L d')
+subtractWithBorrow (A d1 rest1) (A d2 rest2) =
+    let (restBorrow, n') = subtractWithBorrow rest1 rest2
+        (thisBorrow, d') = borrowTable restBorrow d1 d2
+    in (thisBorrow, A d' n')
+
+sub n1 n2 =
+    let (n1', n2')       = pad n1 n2
+        (borrow, result) = subtractWithBorrow n1' n2'
+    in if borrow == True
+       then L Zero  -- We don't have negative numbers in Natnum
+       else result
+
+trunc (L d) = L d
+trunc num@(A d n) =
+    if d == Zero
+    then trunc n
+    else num
 
 append d1 (L d2)   = A d2 (L d1)
 append d1 (A d2 n) = A d2 (append d1 n)
@@ -97,10 +178,11 @@ fromDigits (d:ds) = A d (fromDigits ds)
 
 binary = binary' []
 binary' ds 0 = fromDigits ds
-binary' ds i = let quotient = i `div` 2
-                   rest     = i `mod` 2
-                   d        = if rest == 1 then One else Zero
-               in binary' (d:ds) quotient
+binary' ds i =
+    let quotient = i `div` 2
+        rest     = i `mod` 2
+        d        = if rest == 1 then One else Zero
+    in binary' (d:ds) quotient
 
 size (L _)   = 1
 size (A _ n) = 1 + (size n)
